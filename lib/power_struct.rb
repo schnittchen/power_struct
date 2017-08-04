afbaf32847d9cd4953ed720e322359d9a9ff53e6
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-08-04 13:08:52 +0200

    implement basic behaviour

--- lib/power_struct.rb
+++ lib/power_struct.rb
@@ -1,8 +1,16 @@
 require "power_struct/version"
 
 module PowerStruct
-  def self.new
-    Class.new(Base)
+  def self.new(*attributes)
+    Class.new(Base) do
+      def initialize(*arguments)
+        arguments.first.each do |attribute, value|
+          self.public_send("#{attribute}=", value)
+        end
+      end
+
+      attr_accessor(*attributes)
+    end
   end
 
   class Base
--- spec/power_struct_spec.rb
+++ spec/power_struct_spec.rb
@@ -4,8 +4,29 @@ RSpec.describe PowerStruct do
   describe "an empty power struct" do
     let(:struct) { described_class.new() }
 
-    it "can be instantiated" do
+    it "can be instantiated, or instructs to pass arguments" do
+      pending
       struct.new
+    end
+  end
+
+  describe "a power struct with multiple attributes" do
+    let(:struct) { described_class.new(:foo, :bar) }
+
+    it "initializes with named arguments" do
+      foo = 1
+      bar = 2
+
+      instance = struct.new(foo: foo, bar: bar)
+
+      expect(instance.foo).to be(foo)
+      expect(instance.bar).to be(bar)
+    end
+
+    it "has accessors" do
+      instance = struct.new(foo: nil, bar: nil)
+      instance.foo = :foo
+      expect(instance.foo).to be(:foo)
     end
   end
 end
